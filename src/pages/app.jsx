1c28dbe715d996a542329a9678cc220999c80b44
chedeline <vchedeline@gmail.com>
2022-08-13 10:17:04 -0400

    Login and Register added

---
+++ src/pages/app.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Router } from "@reach/router";
+import Layout from "../components/Layout";
+import Profile from "../components/Profile";
+import PrivateRoute from "../components/PrivateRoute";
+import Login from "../components/Login";
+import Register from "../components/Register";
+import Reset from "../components/Reset";
+
+export default function App() {
+  return (
+    <Layout>
+      <Router>
+        <PrivateRoute path="/app/profile" component={Profile} />
+        <Login path="/app/login" />
+        <Register path="/app/register" />
+        <Reset path="/app/reset" />
+      </Router>
+    </Layout>
+  );
+}
